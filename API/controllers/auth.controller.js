df3a38fb0d76f9c69efeb22af559acc170792aff
Георги Сталев <sttallev99@gmail.com>
2023-09-29 15:42:48 +0300

    Create a middleware and function to handle possible errors

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcryptjs from 'bcryptjs';
 
 import User from '../models/User.model.js';
+import { errorHandler } from '../utils/error.js';
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -10,6 +11,6 @@ export const signUp = async (req, res) => {
         await newUser.save();
         res.status(201).json('User created successfully!');
     }catch(err){
-        res.status(500).json(err.message);
+        next(err)
     }
 }
--- API/index.js
+++ API/index.js
@@ -11,6 +11,15 @@ app.use(express.json());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
 
 mongoose.connect(process.env.MONGO)
     .then(() => console.log('Connected to DB'))
