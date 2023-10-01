4946feb09b99ba66f580e5cde5f06908cf45149a
Георги Сталев <sttallev99@gmail.com>
2023-10-02 02:03:25 +0300

    Complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const { currentUser } = useSelector(state => state.user);
+
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-xl mx-auto'>
+      <h1 className='text-4xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={currentUser.avatar} alt="avatar" className='rounded-full max-h-32 w-32 object-cover 
+        cursor-pointer self-center mt-2'/>
+        <input type="text" placeholder='username'
+        id='username' className='border p-3 rounded-lg'/>
+        <input type="email" placeholder='email'
+        id='email' className='border p-3 rounded-lg'/>
+        <input type="password" placeholder='password'
+        id='password' className='border p-3 rounded-lg'/>
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase
+        hover:opacity-95 disabled:opacity-80'>update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span className='text-red-700 cursor-pointer'>Sign out</span>
+      </div>
+    </div>
   )
 }
