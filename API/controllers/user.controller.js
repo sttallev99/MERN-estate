9eccf1c0388849dde9d1ecf0ce741f081f5bf638
Георги Сталев <sttallev99@gmail.com>
2023-10-03 14:48:50 +0300

    fix typo error

--- API/controllers/user.controller.js
+++ API/controllers/user.controller.js
@@ -20,7 +20,7 @@ export const updateUser = async (req, res, next) => {
                 username: req.body.username,
                 email: req.body.email,
                 password: req.body.password,
-                evatar: req.body.avatar
+                avatar: req.body.avatar
             }
         }, {new: true});
 
