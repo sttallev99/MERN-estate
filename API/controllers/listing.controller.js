00f86375d2a1b04e8c807db23055ffc032e842a9
Георги Сталев <sttallev99@gmail.com>
2023-10-05 21:34:34 +0300

    Create update listing API route

--- API/controllers/listing.controller.js
+++ API/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/Listing.model.js"
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try{
@@ -26,4 +27,22 @@ export const deleteListing = async (req, res, next) => {
     }catch(err) {
         next(err);
     }
-} 
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only update your own listing!'))
+    }
+
+    try{
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json(updatedListing);
+    }catch(err){
+        next(err);
+    }
+}
