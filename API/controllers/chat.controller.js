14e1048487a4ee1fe6de6b0cb4b7e366e2e5535b
Георги Сталев <sttallev99@gmail.com>
2023-10-24 17:17:09 +0300

    create find chat API route

--- API/controllers/chat.controller.js
+++ API/controllers/chat.controller.js
@@ -1,17 +1,19 @@
 import Chat from '../models/Chat.model.js';
 import { errorHandler } from '../utils/error.js';
 
-export const createChat = async (req, res, next) => {
-    const {senderId, receiverId} = req.body;
 
-    if(!senderId || !receiverId) return next(errorHandler(500, 'SenderId and receiverId are required!'))
+//create chat
+export const createChat = async (req, res, next) => {
+    const {firstId, secondId} = req.body;
+
+    if(!firstId || !secondId) return next(errorHandler(500, 'SenderId and receiverId are required!'))
 
     
     try{
-        const chat = await Chat.findOne({members: {$all: [senderId, receiverId]}});
+        const chat = await Chat.findOne({members: {$all: [firstId, secondId]}});
         if(chat) return res.status(200).json(chat);
         
-        const newChat = new Chat({members: [senderId, receiverId]});
+        const newChat = new Chat({members: [firstId, secondId]});
         
         await newChat.save();
         res.status(200).json('Chat created successfully!')
@@ -20,6 +22,7 @@ export const createChat = async (req, res, next) => {
     }
 }
 
+//get user chats
 export const getUserChats = async (req, res, next) => {
     const userId = req.params.userId;
     console.log(userId);
@@ -27,6 +30,19 @@ export const getUserChats = async (req, res, next) => {
     try{
         const chats = await Chat.find({members: { $in: [userId]}});
         res.status(200).json(chats);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+}
+
+//find chat
+export const findChat = async(req, res, next) => {
+    const {firstId, secondId} = req.params;
+
+    try{
+        const chat = await Chat.findOne({members: { $all: [firstId, secondId]}});
+        if(!chat) return next(errorHandler(400, 'chat not found!'));
+        res.status(200).json(chat); 
     }catch(err) {
         res.status(500).json(err);
     }
--- API/routes/chat.router.js
+++ API/routes/chat.router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { createChat, getUserChats } from '../controllers/chat.controller.js';
+import { createChat, findChat, getUserChats } from '../controllers/chat.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/', verifyToken, createChat);
 router.get('/:userId',verifyToken, getUserChats);
+router.get('/find/:firstId/:secondId', verifyToken, findChat);
 
 export default router;
